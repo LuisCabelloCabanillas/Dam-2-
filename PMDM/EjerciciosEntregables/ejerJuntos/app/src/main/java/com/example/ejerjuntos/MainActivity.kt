bfa5327e631cb361afc26d975a293dd428df47ba
Luis Cabello <lcabellocabanillas@safareyes.es>
2025-11-11 00:12:43 +0100

    Ejercicios PMDM

---
+++ PMDM/EjerciciosEntregables/ejerJuntos/app/src/main/java/com/example/ejerjuntos/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ejerjuntos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.Botón1).setOnClickListener {
+            val intent = Intent(this, Actividad1::class.java)
+            startActivity(intent)
+        }
+    }
+}
