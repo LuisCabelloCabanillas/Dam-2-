686455ac8e84bf71dede02a7c0f22b8f6336fabc
Luis Cabello <lcabellocabanillas@safareyes.es>
2026-01-31 20:57:11 +0100

    Front android

--- DI/SchoolEvents/src/app/services/lista-evento.service.ts
+++ DI/SchoolEvents/src/app/services/lista-evento.service.ts
@@ -7,40 +7,28 @@ export class ListaEventosService {
 
   private API = 'https://backend-q6zm.onrender.com';
 
+  constructor() {}
+
   async obtenerEventos(): Promise<Evento[]> {
-    const options = {
-      url: `${this.API}/eventos/todos`,
-      headers: { 'Content-Type': 'application/json' }
-    };
-    const response = await Http.request({ method: 'GET', ...options });
-    return response.data as Evento[];
+    const options = { url: `${this.API}/eventos/todos` };
+    const res = await Http.get(options);
+    return res.data as Evento[];
   }
 
-  async crearEvento(evento: Evento): Promise<Evento> {
+  async detalleEvento(id: number): Promise<Evento> {
+    const options = { url: `${this.API}/eventos/buscar/${id}` };
+    const res = await Http.get(options);
+    return res.data as Evento;
+  }
+
+  async crearEventos(evento: Evento): Promise<Evento> {
     const options = {
       url: `${this.API}/eventos/crear`,
       headers: { 'Content-Type': 'application/json' },
       data: evento
     };
-    const response = await Http.request({ method: 'POST', ...options });
-    return response.data as Evento;
-  }
-
-  async eliminarEvento(id: number): Promise<void> {
-    const options = {
-      url: `${this.API}/eventos/eliminar/${id}`,
-      headers: { 'Content-Type': 'application/json' }
-    };
-    await Http.request({ method: 'DELETE', ...options });
-  }
-
-  async detalleEvento(id: number): Promise<Evento> {
-    const options = {
-      url: `${this.API}/eventos/buscar/${id}`,
-      headers: { 'Content-Type': 'application/json' }
-    };
-    const response = await Http.request({ method: 'GET', ...options });
-    return response.data as Evento;
+    const res = await Http.post(options);
+    return res.data as Evento;
   }
 
   async editarEvento(id: number, evento: Evento): Promise<Evento> {
@@ -49,7 +37,12 @@ export class ListaEventosService {
       headers: { 'Content-Type': 'application/json' },
       data: evento
     };
-    const response = await Http.request({ method: 'PUT', ...options });
-    return response.data as Evento;
+    const res = await Http.put(options);
+    return res.data as Evento;
+  }
+
+  async eliminarEvento(id: number): Promise<void> {
+    const options = { url: `${this.API}/eventos/eliminar/${id}` };
+    await Http.del(options);
   }
 }
