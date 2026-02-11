4528755495f0eac05dbc085a0465cdb34b06ab1e
Luis Cabello <luisitocabcabanillas@gmail.com>
2026-02-11 12:29:45 +0100

    django

--- Ac_Datos/AcDatosParaMongo/safarank/urls.py
+++ Ac_Datos/AcDatosParaMongo/safarank/urls.py
@@ -4,7 +4,7 @@ from safarank import views
 urlpatterns = [
     path('', views.select_user ,name='Select_conec'),
     path('inicio/', views.go_home, name='go_home'),
-    path('juegos/', views.rankings, name='rankings'),
+    path('juegos/', views.rankings, name='Rankings'),
     path('login/', views.do_login, name='do_login'),
     path('logout/', views.logout_user, name='logout_user'),
     path('datos/', views.data_load, name='data_load'),
