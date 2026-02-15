b30c72ec953b1e5409d5fa601b97a6231f9c0c4b
Luis Cabello <lcabellocabanillas@safareyes.es>
2026-02-15 20:01:01 +0100

    Arreglo seguridad

--- DI/SchoolEvents/src/main.ts
+++ DI/SchoolEvents/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader'; // Fíjate en el @
+
+defineCustomElements(window);
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
